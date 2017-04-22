958876c08cffa197530205797ed793658bde637e
JimmyJ <jimmyj.inbox@gmail.com>
2017-04-21 20:41:52 -0500

    changed password mismatch

--- controller/rating_controller.js
+++ controller/rating_controller.js
@@ -8,16 +8,106 @@ var session = require('express-session');
 var rating = {
 
 troll: function(req, res){
-req.session.reset();
-console.log("signed out!")
-  res.redirect('/');
+	console.log("Inside of Troll");
+	console.log(req.body)
+var trollPost = {
+  receiverUserID: req.body.senderUserID,
+  senderUserID: req.session.user[0][0].userID,
+  troll: true
+}
+
+var options = {
+    method: 'POST',
+    uri: 'http://echoingwallapiservice.azurewebsites.net/save/rating',
+    body: trollPost,
+    json: true // Automatically stringifies the body to JSON 
+};
+ 
+        rp(options)
+            .then(function (response) {
+              
+                // POST succeeded... 
+                      if(response.status == 200){
+
+                        console.log(200+" inside Like");
+
+                        
+                       res.redirect("/startChat1") 
+                    
+                     }else{
+                         console.log(500);
+                          res.redirect("/errorPage");
+                     }
+    })
+    .catch(function (err) {
+          res.redirect("/errorPage");
+    });
+
+
 },
 
 like: function(req, res){
-req.session.reset();
-console.log("signed out!")
-  res.redirect('/');
+	console.log("Inside of Like")
+	console.log(req.body)
+
+
+var likePost = {
+  receiverUserID: req.body.senderUserID,
+  senderUserID: req.session.user[0][0].userID,
+  troll: false
 }
+
+
+
+
+var options = {
+    method: 'POST',
+    uri: 'http://echoingwallapiservice.azurewebsites.net/save/rating',
+    body: likePost,
+    json: true // Automatically stringifies the body to JSON 
+};
+ 
+        rp(options)
+            .then(function (response) {
+              
+                // POST succeeded... 
+                      if(response.status == 200){
+
+                        console.log(200+" inside Like");
+                        
+                       res.redirect("/startChat1") 
+                    
+                     }else{
+                         console.log(500);
+                          res.redirect("/errorPage");
+                     }
+    })
+    .catch(function (err) {
+          res.redirect("/errorPage");
+    });
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
